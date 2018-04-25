f8128f36f24942cd51592d99dee6045518e6fbab
Steele Walston <steele.walston@gmail.com>
2018-04-25 10:37:53 -0700

    tests on bad email and bad password work

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -35,5 +35,41 @@ describe.only('Auth API', () => {
             });
     });
 
+    it('Gives 400 on signup of same email', () => {
+        return request
+            .post('/auth/signup')
+            .send(bob)
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'email exists');
+            });
+    });
+
+    it('Gives 401 on non-existent email', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                email: 'bad@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+
+    it('Gives 401 on bad password', () => {
+        return request
+            .post('/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'bad'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+
 });
 
